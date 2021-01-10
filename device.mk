841d249a38b524a592cae6080f204deac3da1782
Jabiyeff <cebiyevanar@gmail.com>
2021-01-10 17:10:09 +0400

    santoni: Build basic USB HAL from source
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -492,7 +492,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.santoni
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \

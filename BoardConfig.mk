aa542afca7b0584aa729eb50fa90d54900a02375
Jabiyeff <cebiyevanar@gmail.com>
2021-01-14 21:51:47 +0400

    santoni: sepolicy: address iorap denials
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,6 +227,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
